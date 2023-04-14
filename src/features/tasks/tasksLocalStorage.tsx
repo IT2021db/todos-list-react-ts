896b6cbc71857811dc9b50ad2f70ead75cb6d714
Dorota Borowska <youcode2021db@gmail.com>
2023-04-14 17:53:15 +0200

    iterface Task moved to types.ts

--- src/features/tasks/tasksLocalStorage.tsx
+++ src/features/tasks/tasksLocalStorage.tsx
@@ -1,13 +1,10 @@
+import { Task } from "./types";
+
 const localStorageKey: string = "tasks";
 
 export const saveTasksInLocalStorage = (tasks?: string): void =>
   localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
-  interface Task {
-    id: number;
-    content: string;
-    done: boolean;
-  }
 export const getTasksFromLocalStorage = (): Task[] => {
   const tasks = localStorage.getItem(localStorageKey);
   return tasks ? JSON.parse(tasks) : [];
