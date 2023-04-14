1986a13603dd0510444954b1b768419b2d5e6d45
Dorota Borowska <youcode2021db@gmail.com>
2023-04-14 18:27:47 +0200

    onInputChange has got TS ChangeEvent interface

--- src/features/tasks/TasksPage/Search/index.tsx
+++ src/features/tasks/TasksPage/Search/index.tsx
@@ -1,27 +1,33 @@
 import React, { ChangeEvent } from "react";
 import { Wrapper } from "./styled";
 import Input from "../../Input";
-import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../queryParameters";
 import searchQueryParamName from "../searchQueryParamName";
 
 export default () => {
-    const query = useQueryParameter(searchQueryParamName);
-    const replaceQueryParameter = useReplaceQueryParameter();
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
 
-    const onInputChange = (event:ChangeEvent<HTMLInputElement>|any) => {
-        replaceQueryParameter({
-            key: searchQueryParamName,
-            value: event.target.value.trim() !== "" ? event.target.value : undefined,
-        });
-    };
+  const onInputChange = (event: ChangeEvent<HTMLInputElement> | any) => {
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: event.target.value.trim() !== "" ? event.target.value : undefined,
+    });
+  };
 
-    return (
-        <Wrapper>
-            <Input
-                placeholder="Filtruj zadania"
-                value={query || ""}
-                onChange={onInputChange}
-            />
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <Input
+        placeholder="Filtruj zadania"
+        value={query || ""}
+        onChange={onInputChange}
+      />
+    </Wrapper>
+  );
 };
+// The ChangeEvent interface is generic, with the type parameter representing 
+// the type of the target property. In this case, the target property is of type HTMLInputElement, 
+// so the type parameter is HTMLInputElement.
