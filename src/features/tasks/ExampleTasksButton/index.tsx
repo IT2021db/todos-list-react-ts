b2cc71dd49cfd53589f2660cfbb027e183cb8885
Dorota Borowska <youcode2021db@gmail.com>
2023-04-14 14:13:39 +0200

    formatted

--- src/features/tasks/ExampleTasksButton/index.tsx
+++ src/features/tasks/ExampleTasksButton/index.tsx
@@ -4,21 +4,18 @@ import { Buttons } from "../TasksPage/Buttons/styled";
 import { fetchExampleTasks, selectLoading } from "../tasksSlice";
 
 export const ExampleTasksButton = () => {
-    const loading = useSelector(selectLoading)
-    const dispatch = useDispatch();
-    return (
-        <div>
-            <>
-                <Buttons
-                    onClick={() => dispatch(fetchExampleTasks())}
-                    disabled={loading}
-                >
-                    {loading
-                        ? "Ładowanie..."
-                        : "Pobierz przykładowe zadania"
-                    }
-                </Buttons>
-            </>
-        </div>
-    )
-}
+  const loading = useSelector(selectLoading);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <>
+        <Buttons
+          onClick={() => dispatch(fetchExampleTasks())}
+          disabled={loading}
+        >
+          {loading ? "Ładowanie..." : "Pobierz przykładowe zadania"}
+        </Buttons>
+      </>
+    </div>
+  );
+};
