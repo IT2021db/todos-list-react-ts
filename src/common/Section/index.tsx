f900568b95249e773135cf555cb53a4b2b1e18cf
Dorota Borowska <youcode2021db@gmail.com>
2023-04-14 17:51:56 +0200

    formatted, type React.ReactNode added

--- src/common/Section/index.tsx
+++ src/common/Section/index.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { Header, Wrapper } from "./styled";
 
-interface SectionProps{
-  title: string|null;
+interface SectionProps {
+  title: string | null;
   body?: React.ReactNode;
   extraHeaderElement?: React.ReactNode;
 }
 
-const Section = ({ title, body, extraHeaderElement }:SectionProps) => (
+const Section = ({ title, body, extraHeaderElement }: SectionProps) => (
   <Wrapper>
-    <Header  >
-      <h2 >{title}</h2>
-      <div>
-        {extraHeaderElement}
-      </div>
+    <Header>
+      <h2>{title}</h2>
+      <div>{extraHeaderElement}</div>
     </Header>
-    <div>
-      {body}
-    </div>
+    <div>{body}</div>
   </Wrapper>
 );
 
 export default Section;
+
+// React.ReactNode is a type in React that represents the possible types
+// that a React component can return.
+// It is a union type that includes all the possible return types
+// of a React component, such as elements, fragments, strings, numbers,
+// booleans, arrays, and null or undefined.
+
+// The React.ReactNode type is used in TypeScript to ensure that
+//  a component's return value conforms to the expected types in the React element tree.
