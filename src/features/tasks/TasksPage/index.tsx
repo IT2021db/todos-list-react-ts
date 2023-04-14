9a7edf1ecbde3ecaf7f6f39551ed32a47b50d061
Dorota Borowska <youcode2021db@gmail.com>
2023-04-14 17:57:42 +0200

    code fromatted

--- src/features/tasks/TasksPage/index.tsx
+++ src/features/tasks/TasksPage/index.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
-import Buttons from './Buttons';
-import Form from './Form';
-import TasksList from './TasksList';
-import Section from '../../../common/Section';
-import Header from '../../../common/Header';
-import Container from '../../../common/Container';
-import { ExampleTasksButton } from '../ExampleTasksButton';
-import Search from './Search';
+import React from "react";
+import Buttons from "./Buttons";
+import Form from "./Form";
+import TasksList from "./TasksList";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Container from "../../../common/Container";
+import { ExampleTasksButton } from "../ExampleTasksButton";
+import Search from "./Search";
 
 function TasksPage() {
   return (
     <Container>
-      <Header
-        title="Lista zadań"
-      />
+      <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
         body={<Form />}
         extraHeaderElement={<ExampleTasksButton />}
       />
-      <Section
-        title="Wyszukiwarka"
-        body={<Search />}
-      />
+      <Section title="Wyszukiwarka" body={<Search />} />
       <Section
         title="Lista zadań"
         body={<TasksList />}
@@ -33,4 +28,3 @@ function TasksPage() {
 }
 
 export default TasksPage;
-
