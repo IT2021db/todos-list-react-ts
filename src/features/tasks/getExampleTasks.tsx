c0e3051dde6c108c38b0719b538555f79b52b78f
Dorota Borowska <youcode2021db@gmail.com>
2023-04-15 18:05:34 +0200

    formatted

--- src/features/tasks/getExampleTasks.tsx
+++ src/features/tasks/getExampleTasks.tsx
@@ -1,9 +1,7 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("/todos-list-react/exampleTasks.json");
-
-    if (!response.ok) {
-        new Error(response.statusText);
-    }
-
-    return await response.json();
+  const response = await fetch("/todos-list-ts/exampleTasks.json");
+  if (!response.ok) {
+    new Error(response.statusText);
+  }
+  return await response.json();
 };
