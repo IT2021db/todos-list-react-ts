0074cfb7b9435915e866408b8eb133d44c945f59
Dorota Borowska <youcode2021db@gmail.com>
2023-04-21 19:46:21 +0200

    async typed with Promise<task[]>

--- src/features/tasks/getExampleTasks.tsx
+++ src/features/tasks/getExampleTasks.tsx
@@ -1,4 +1,6 @@
-export const getExampleTasks = async () => {
+import {Task}from "../../types";
+
+export const getExampleTasks = async (): Promise<Task[]> => {
   const response = await fetch("/todos-list-ts/exampleTasks.json");
   if (!response.ok) {
     new Error(response.statusText);
