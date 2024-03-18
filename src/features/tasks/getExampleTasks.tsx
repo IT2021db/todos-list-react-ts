7fd51ee3a89f2e1e4de69ec705bab2046f899dde
Dorota Borowska <youcode2021db@gmail.com>
2024-03-18 15:38:03 +0100

    Fix: change expected type

--- src/features/tasks/getExampleTasks.tsx
+++ src/features/tasks/getExampleTasks.tsx
@@ -1,9 +1,7 @@
-import {Task}from "../../types";
-
-export const getExampleTasks = async (): Promise<Task[]> => {
+export const getExampleTasks = async () => {
   const response = await fetch("/todos-list-ts/exampleTasks.json");
   if (!response.ok) {
     new Error(response.statusText);
   }
-  return await response.json();
+  return (await response.json()) as string[];
 };
