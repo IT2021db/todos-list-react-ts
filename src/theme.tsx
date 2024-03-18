ef43932c1f40b30bba17ea391dde58bf7dda1e59
Dorota Borowska <youcode2021db@gmail.com>
2024-03-18 14:41:08 +0100

    Fix: restore previous version of theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,23 +1,23 @@
-import {IndexSignature}from './types';
-export interface Theme {
-    color: IndexSignature;
-    breakpoint: {
-        mobileMax: number;
-    };
-}
+// import {IndexSignature}from './types';
+// export interface Theme {
+//     color: IndexSignature;
+//     breakpoint: {
+//         mobileMax: number;
+//     };
+// }
 
-export const theme: Theme = {
-    color: {
-        black: "#000",
-        silver: "#ccc",
-        alto: "#ddd",
-        gallery: "#eee",
-        white: "#fff",
-        teal: "#008080",
-        cardinal: "#c41e3a",
-        forestGreen: "#228c22",
-    },
-    breakpoint: {
-        mobileMax: 767,
-    },
-};
+export const theme = {
+  color: {
+    black: "#000",
+    silver: "#ccc",
+    alto: "#ddd",
+    gallery: "#eee",
+    white: "#fff",
+    teal: "#008080",
+    cardinal: "#c41e3a",
+    forestGreen: "#228c22",
+  },
+  breakpoint: {
+    mobileMax: 767,
+  },
+} as const;
